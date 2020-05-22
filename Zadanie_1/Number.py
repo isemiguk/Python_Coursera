045b16d89f47e5d524ee1cbb1589ac33a56eee26
I_Semiguk <igorsemiguk@gmail.com>
2020-05-22 23:29:45 +0300

    Add Number

---
+++ Zadanie_1/Number.py
@@ -0,0 +1,15 @@
+from datetime import time
+
+print(11 + 6)
+print(11 - 6)
+print(11 * 6)
+print(11**6) #возведение в степень
+print(11 / 6)
+print(11 // 6) #целочисленное деление
+print(11 % 6) #остаток от деления
+
+#работа с переменными
+speed = 100
+time = 8
+dist = speed * time
+print(dist)
